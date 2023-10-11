86cccf29cc50ea07bd320de58820d7664c9c3b6e
octochamp <49677546+octochamp@users.noreply.github.com>
2023-10-11 14:17:54 +0100

    All basic functionality including basic graphics working.

--- bangleBridge.js
+++ bangleBridge.js
@@ -23,6 +23,7 @@ async function selectFile() {
 
 async function startChecking() {
     checkProcess();
+    Puck.write("Bangle.setLCDPower(1); \n Bangle.setLCDTimeout(0);")
     interval = 5000;
     intervalId = setInterval(checkProcess, interval); // Re-read the .txt file every 5 seconds
     return;
@@ -40,10 +41,12 @@ async function checkProcess() {
 
             if (fileText === "true") {
                 console.log("Text says TRUE");
+                Puck.write("g.setBgColor(0,1,0); \n g.clear(); \n g.setFont('Vector', 60); \n g.drawString('True');")
                 return;
             } else if (fileText === "false") {
                 console.log("Text says FALSE")
-                Puck.write("Bangle.buzz()\n")
+                Puck.write("g.setBgColor(1,0,0); \n g.clear(); \n g.setFont('Vector', 60); \n g.drawString('False');")
+                Puck.write("Bangle.buzz();\n Bangle.buzz();\n Bangle.buzz();\n")
                 return;
             } else {
                 console.log("Error: Invalid input (neither true nor false)");
@@ -60,6 +63,7 @@ async function checkProcess() {
 
 function stopChecking() {
     clearInterval(intervalId);
+    Puck.write("Bangle.setLCDPower(0.5); \n Bangle.setLCDTimeout(5);")
     document.getElementById("startButton").removeAttribute("disabled");
     document.getElementById("stopButton").setAttribute("disabled", "");
 }
