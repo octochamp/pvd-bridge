03fbdaa6a848304120c2e446dfbbab47dfa9eefd
octochamp <49677546+octochamp@users.noreply.github.com>
2023-10-11 13:20:09 +0100

    separated functions to buttons, fixed issues where the functions ran continuously or  without being triggered

--- bangleBridge.js
+++ bangleBridge.js
@@ -1,5 +1,5 @@
 let fileHandle; // Store the reference to the file handle
-let interval = 5000;
+let interval;
 
 function connectBangle() {
     let connected = false;
@@ -11,37 +11,55 @@ function connectBangle() {
     });
 }
 
-const stopChecking = () => {clearInterval(intervalId)}
+async function selectFile() {
+    try {
+        fileHandle = await window.showOpenFilePicker();
+        document.getElementById("startButton").removeAttribute("disabled");
+    } catch (error) {
+        console.error('Error selecting file: ' + error.message);
+    }
+    return;
+}
 
-async function checkAiOutput() {
-    if (!fileHandle) {
+async function startChecking() {
+    checkProcess();
+    interval = 5000;
+    intervalId = setInterval(checkProcess, interval); // Re-read the .txt file every 5 seconds
+    return;
+}
+
+async function checkProcess() {
+     if (fileHandle != 0) {
+        document.getElementById("stopButton").removeAttribute("disabled");
+        document.getElementById("startButton").setAttribute("disabled", "");
         try {
-            fileHandle = await window.showOpenFilePicker();
+            const file = await fileHandle[0].getFile();
+            const blob = await file.text(); // Read the contents of the file as a Blob
+
+            const fileText = await new Response(blob).text();
+
+            if (fileText === "true") {
+                console.log("Text says TRUE");
+                return;
+            } else if (fileText === "false") {
+                console.log("Text says FALSE")
+                Puck.write("Bangle.buzz()\n")
+                return;
+            } else {
+                console.log("Error: Invalid input (neither true nor false)");
+                return;
+            }
         } catch (error) {
-            console.error('Error selecting file: ' + error.message);
+            console.error('Error fetching file: ' + error.message);
             return;
         }
-    }
-
-    try {
-        const file = await fileHandle[0].getFile();
-        const blob = await file.text(); // Read the contents of the file as a Blob
-
-        const fileText = await new Response(blob).text();
-
-        if (fileText === "true") {
-            console.log("Text says TRUE");
-        } else if (fileText === "false") {
-            console.log("Text says FALSE")
-            Puck.write("Bangle.buzz()\n")
-            ;
-        } else {
-            console.log("Error: Invalid input (neither true nor false)");
-        }
-    } catch (error) {
-        console.error('Error fetching file: ' + error.message);
+    } else {
+        return;
     }
 }
 
-// Set up a periodic timer to re-read the file
-intervalId = setInterval(checkAiOutput, interval); // Re-read the file every 5 seconds
+function stopChecking() {
+    clearInterval(intervalId);
+    document.getElementById("startButton").removeAttribute("disabled");
+    document.getElementById("stopButton").setAttribute("disabled", "");
+}
