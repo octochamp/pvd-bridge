6fca58c9cf1268f2ad1f16de2cde18bf9450ae79
octochamp <49677546+octochamp@users.noreply.github.com>
2023-10-11 02:01:16 +0100

    Now connects to Bangle.js and buzzes if result is False

--- bangleBridge.js
+++ bangleBridge.js
@@ -1,7 +1,17 @@
 let fileHandle; // Store the reference to the file handle
 let interval = 1000;
 
-const stopChecking = () => {clearInterval(intervalId)};
+function connectBangle() {
+    let connected = false;
+    let pickCounter = 0;
+    Puck.modal(function () {
+        Puck.write("\n", function () {
+            connected = true;
+        });
+    });
+}
+
+const stopChecking = () => {clearInterval(intervalId)}
 
 async function checkAiOutput() {
     if (!fileHandle) {
@@ -22,7 +32,9 @@ async function checkAiOutput() {
         if (fileText === "true") {
             console.log("Text says TRUE");
         } else if (fileText === "false") {
-            console.log("Text says FALSE");
+            console.log("Text says FALSE")
+            Puck.write("Bangle.buzz()\n")
+            ;
         } else {
             console.log("Error: Invalid input (neither true nor false)");
         }
